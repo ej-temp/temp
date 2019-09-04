6487dfa268751397fe78159c0f0c45dfe80adf53
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-09-04 15:45:06 -0700

    add fetch actions

--- src/actions/temps.js
+++ src/actions/temps.js
@@ -1,7 +1,19 @@
-import { getAllTemps } from '../services/tempApi';
+import { getAllTemps, getHottestTemp, getColdestTemp } from '../services/tempApi';
 
 export const FETCH_TEMPS = 'FETCH_TEMPS';
 export const fetchTemps = () => ({
   type: FETCH_TEMPS,
   payload: getAllTemps()
+});
+
+export const FETCH_HIGH_TEMP = 'FETCH_HIGH_TEMP';
+export const fetchHighTemp = () => ({
+  type: FETCH_HIGH_TEMP,
+  payload: getHottestTemp()
+});
+
+export const FETCH_LOW_TEMP = 'FETCH_LOW_TEMP';
+export const fetchLowTemp = () => ({
+  type: FETCH_LOW_TEMP,
+  payload: getColdestTemp()
 });
