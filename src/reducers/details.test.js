6e2b796fe1f0df44c6165a4a5cf5861d67ca2bc3
Jackmc <jackmc1122@gmail.com>
2019-09-05 09:32:33 -0700

    Finish testing the detail reducer

--- src/reducers/details.test.js
+++ src/reducers/details.test.js
@@ -1,5 +1,5 @@
 import reducer from './details';
-import { FETCH_DETAIL } from '../actions/temps';
+import { FETCH_DETAIL, CLEAR_DETAIL } from '../actions/temps';
 
 describe('details reducer', () => {
   it('handles the fetch detail action', () => {
@@ -16,5 +16,21 @@ describe('details reducer', () => {
     expect(newState).toEqual({ 
       name: 'hi', 
       temps: [26, 57, 98, 24] });
+  });
+
+  it('handles the clear detail action', () => {
+    const initialState = {
+      name: 'hi', 
+      temps: [26, 57, 98, 24]
+    };
+
+    const newState = reducer(initialState, {
+      type: CLEAR_DETAIL
+    });
+
+    expect(newState).toEqual({ 
+      temps: [],
+      name: ''
+    });
   });
 });
